e717788c2090c14e539d2bd691663e2ae2083488
ahonculada <achonculada@gmail.com>
2022-02-04 00:18:15 -0500

    added views manager

--- README.md
+++ README.md
@@ -5,5 +5,11 @@
 ~~2. Build a factory that enables the structs to be built in the to_do module.~~  
 ~~3. Build traits that enable a struct to delete, create, edit, and get the to-do items.~~  
 ~~4. Build a read and write to file module to be utilized by other modules.~~  
-5. Build a config module that can alter the behavior of the app based on the environment.  
+~~5. Build a config module that can alter the behavior of the app based on the environment.~~  
+
+6. Load the current state of the to do item list.  
+7. Get the title of the new to do item from the URL.  
+8. Pass the title and the 'pending' string through 'to_do_factory'.  
+9. Pass the result of the previous step, along with the 'create' string and the state, into the process module interface.  
+10. Return a string to the user to signal that the process has finished.  
 
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,16 @@
+use actix_web::{App, HttpServer};
 mod to_do;
 mod state;
+mod views;
 mod processes;
 
-use std::env;
-use state::read_file;
-use serde_json::value::Value;
-use serde_json::Map;
-use to_do::to_do_factory;
-use processes::process_input;
-
-fn main() {
-    // collects arguments passed into the program
-    let args: Vec<String> = env::args().collect();
-
-    // defines the commands from the environment
-    let command: &String = &args[1];
-    let title: &String = &args[2];
-
-    // read the JSON file and print it using the debug notation
-    let state: Map<String, Value> = read_file(
-        String::from("./state.json")
-    );
-
-    // define outside the match scope to enable using status later on
-    let status: String;
-
-    // check to see if the title is alread there, setting status to pending if not
-    match &state.get(*&title) {
-        Some(result) => {
-            status = result.to_string().replace('\"', "");
-        }
-        None => {
-            status = String::from("pending");
-        }
-    }
-    // create a to do struct depending on status
-    let item = to_do_factory(&status, title.to_string()).expect(&status);
-
-    // update state based on struct and command
-    process_input(item, command.to_string(), &state);
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        let app = App::new().configure(views::views_factory);
+        return app
+    })
+    .bind("127.0.0.1:8000")?
+        .run()
+        .await
 }
